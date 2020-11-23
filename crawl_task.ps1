bcaadde481ada9d6a551442e705d48cf389d2a00
Jiseok Oh <jiseok95@gmail.com>
2020-11-24 08:25:03 +0900

    Other: url update

--- crawl_task.ps1
+++ crawl_task.ps1
@@ -2,7 +2,7 @@ cd C:\Users\copyleft\crawlCopyright
 .venv\Scripts\activate
 python .\crawl.py --force-crawl=True https://podo11.com/ 
 python .\crawl.py --force-crawl=True https://linkmoum.live/ 
-python .\crawl.py --force-crawl=True https://jusomoya.com/
+python .\crawl.py --force-crawl=True https://jusomoya1.com/
 python .\crawl.py --force-crawl=True https://linkzip.site
 python .\crawl.py --force-crawl=True https://linkpan22.com/
 python .\get_site_information.py available
